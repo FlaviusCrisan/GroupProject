b3a7841baae6971016b7969e4fab7cf56fbae63b
totally real name <153068435+maks-ymilian@users.noreply.github.com>
2026-02-23 09:34:14 +0000

    add sidebar

--- client/src/app/pages/home/home.ts
+++ client/src/app/pages/home/home.ts
@@ -1,12 +1,15 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 import { register } from 'swiper/element/bundle';
 register();
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, MatButtonModule],
+  imports: [RouterModule, MatButtonModule, MatSidenavModule, MatListModule, MatIconModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
