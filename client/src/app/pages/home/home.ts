6df85c69e343a283ca794c0f266265d095300845
totally real name <153068435+maks-ymilian@users.noreply.github.com>
2026-03-05 10:39:05 +0000

    implment posting and getting on frontend

--- client/src/app/pages/home/home.ts
+++ client/src/app/pages/home/home.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -7,30 +8,44 @@ import { MatIconModule } from '@angular/material/icon';
 import { register } from 'swiper/element/bundle';
 import { firstValueFrom } from 'rxjs';
 import { ApiService } from '../../services/api.service';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 register();
 
 @Component({
   selector: 'app-home',
-  imports: [RouterModule, MatButtonModule, MatSidenavModule, MatListModule, MatIconModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatSidenavModule, MatListModule, MatIconModule, FormsModule, MatInputModule, MatFormFieldModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class Home implements OnInit {
 
+  text : string = "";
+  posts : any[] = [];
+
   constructor(private api: ApiService) {}
 
   async ngOnInit(): Promise<void>
   {
-    let res = await firstValueFrom(this.api.delete_all_posts());
-    res = await firstValueFrom(this.api.get_games());
+    this.load_posts();
+  }
+
+  async load_posts()
+  {
+    let res = await firstValueFrom(this.api.get_games());
+	this.posts = res as any[];
+    console.log(res);
+	return res;
+  }
+
+  async add()
+  {
+    let res = await firstValueFrom(this.api.post_game(this.text));
 	console.log(res);
-    res = await firstValueFrom(this.api.post_game("post1"));
-    res = await firstValueFrom(this.api.get_games());
-	console.log(res);
-    res = await firstValueFrom(this.api.post_game("post2"));
-    res = await firstValueFrom(this.api.get_games());
-	console.log(res);
+	this.text = "";
+	await this.load_posts();
   }
 
 }
