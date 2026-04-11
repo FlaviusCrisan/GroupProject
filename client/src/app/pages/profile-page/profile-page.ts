3f1f20fc3870b68807a0919ca5b1a2b7c2df2946
totally real name <153068435+maks-ymilian@users.noreply.github.com>
2026-04-11 22:12:50 +0100

    add edit profile button

--- client/src/app/pages/profile-page/profile-page.ts
+++ client/src/app/pages/profile-page/profile-page.ts
@@ -14,17 +14,24 @@ import { MatButtonModule } from '@angular/material/button';
 export class ProfilePage implements OnInit
 {
   user_info?: any;
+  is_self?: boolean;
 
   constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, private cd: ChangeDetectorRef) {}
 
   async ngOnInit()
   {
     this.user_info = await this.api.get_user_info(this.route.snapshot.paramMap.get("id")!);
+    this.is_self = this.user_info.id === await this.api.get_user_id();
     this.cd.detectChanges();
   }
 
   open_dms()
   {
     this.router.navigate(['user', this.user_info?.id, 'dms']);
+  }
+
+  edit_profile()
+  {
+    window.location.href = "https://climbing-liger-76.accounts.dev/user";
   }
 }
