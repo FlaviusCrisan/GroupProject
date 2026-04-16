ef63a3316dc250698fa398defe1855facca2569c
FlaviusCrisan <162991075+FlaviusCrisan@users.noreply.github.com>
2026-04-16 05:51:37 +0100

    Login Page Test Cases Updated
    - Ensures Login works as expected through email 
    
    - Ensures properties are visible and display the correct content

--- playwright-testing/tests/login.spec.ts
+++ playwright-testing/tests/login.spec.ts
@@ -5,11 +5,25 @@ test.describe('Login Page', () => {
     test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200');
     });
-
+    
     test('page loads with correct button', async ({ page }) => {
         const button = page.getByRole('button', {name: 'Continue' });
         await expect(button).toBeVisible();
     });
+    
+    test('logs in through email + password flow', async ({ page }) => {
+        await page.goto('http://localhost:4200');
+      
+        await page.getByPlaceholder('Enter email or username').fill('lamudu@denipl.com');
+        await page.getByRole('button', { name: 'Continue' }).click();
+      
+        await expect(page).toHaveURL(/#\/factor-one/);
+        await page.locator('input[type="password"]').fill('pAsSwOrD123_');
+        await page.getByRole('button', { name: /continue|sign in|login/i }).click();
+        await expect(page).toHaveURL(/#\/factor-two/);
+        await page.pause();
+        await expect(page).toHaveURL('http://localhost:4200/home');
+      });
 
     test('user is able to type in the email field', async ({ page }) => {
         const input = page.getByPlaceholder('Enter email or username');
@@ -21,8 +35,9 @@ test.describe('Login Page', () => {
         await expect(page.getByText('Sign up')).toBeVisible();
     });
 
-    test('header has correct text', async ({ page }) => {
-        await expect(page.getByRole('heading', {name: 'Sign in to Project Management'}))
+    test('header has correct text and is visible', async ({ page }) => {
+        const header = page.getByRole('heading', { name: 'Sign in to Project Management' });
+        await expect(header).toBeVisible();
     });
 
     test('welcome subtext is visible', async ({ page }) => {
@@ -34,9 +49,7 @@ test.describe('Login Page', () => {
     });
 
     test('footer question is visible', async ({ page }) => {
-        await expect(page.getByText("Don't have an account?")).toBeVisible();
+        await expect(page.getByText(/don.?t have an account/i)).toBeVisible();
     });
-
-
 
 });
